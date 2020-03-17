0f48fb217e42d43efae8daae2d7e867b64ee323d
Huseyin Kaynar <hkaynar@admin-MacBook-Pro.local>
2020-03-17 03:22:53 +0300

    firs save

---
+++ src/main/java/flightbooking/com/example/flightbooking/Model/Airport.java
@@ -0,0 +1,16 @@
+package flightbooking.com.example.flightbooking.Model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Airport {
+
+
+
+}
---
+++ src/main/java/flightbooking/com/example/flightbooking/Model/Route.java
@@ -0,0 +1,4 @@
+package flightbooking.com.example.flightbooking.Model;
+
+public class Route {
+}
